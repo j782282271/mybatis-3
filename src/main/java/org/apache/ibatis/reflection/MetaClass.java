937eaa9764d7de920fd3b822740c618bf2750a88
j782282271 <782282271@qq.com>
2019-06-15 10:46:30 +0800

    注释

--- src/main/java/org/apache/ibatis/reflection/MetaClass.java
+++ src/main/java/org/apache/ibatis/reflection/MetaClass.java
@@ -1,19 +1,24 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection;
+
+import org.apache.ibatis.reflection.invoker.GetFieldInvoker;
+import org.apache.ibatis.reflection.invoker.Invoker;
+import org.apache.ibatis.reflection.invoker.MethodInvoker;
+import org.apache.ibatis.reflection.property.PropertyTokenizer;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -21,174 +26,232 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Collection;
 
-import org.apache.ibatis.reflection.invoker.GetFieldInvoker;
-import org.apache.ibatis.reflection.invoker.Invoker;
-import org.apache.ibatis.reflection.invoker.MethodInvoker;
-import org.apache.ibatis.reflection.property.PropertyTokenizer;
-
 /**
+ * MetaClass封装了Reflector，比Reflector功能更丰富，丰富的功能：
+ * 能处理orders[0].items[0].name这种递归子类
+ *
  * @author Clinton Begin
  */
 public class MetaClass {
 
-  private ReflectorFactory reflectorFactory;
-  private Reflector reflector;
+    private ReflectorFactory reflectorFactory;
+    private Reflector reflector;
 
-  private MetaClass(Class<?> type, ReflectorFactory reflectorFactory) {
-	this.reflectorFactory = reflectorFactory;
-    this.reflector = reflectorFactory.findForClass(type);
-  }
-
-  public static MetaClass forClass(Class<?> type, ReflectorFactory reflectorFactory) {
-    return new MetaClass(type, reflectorFactory);
-  }
-
-  public MetaClass metaClassForProperty(String name) {
-    Class<?> propType = reflector.getGetterType(name);
-    return MetaClass.forClass(propType, reflectorFactory);
-  }
-
-  public String findProperty(String name) {
-    StringBuilder prop = buildProperty(name, new StringBuilder());
-    return prop.length() > 0 ? prop.toString() : null;
-  }
-
-  public String findProperty(String name, boolean useCamelCaseMapping) {
-    if (useCamelCaseMapping) {
-      name = name.replace("_", "");
+    private MetaClass(Class<?> type, ReflectorFactory reflectorFactory) {
+        this.reflectorFactory = reflectorFactory;
+        this.reflector = reflectorFactory.findForClass(type);
     }
-    return findProperty(name);
-  }
 
-  public String[] getGetterNames() {
-    return reflector.getGetablePropertyNames();
-  }
-
-  public String[] getSetterNames() {
-    return reflector.getSetablePropertyNames();
-  }
-
-  public Class<?> getSetterType(String name) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
-      MetaClass metaProp = metaClassForProperty(prop.getName());
-      return metaProp.getSetterType(prop.getChildren());
-    } else {
-      return reflector.getSetterType(prop.getName());
+    public static MetaClass forClass(Class<?> type, ReflectorFactory reflectorFactory) {
+        return new MetaClass(type, reflectorFactory);
     }
-  }
 
-  public Class<?> getGetterType(String name) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
-      MetaClass metaProp = metaClassForProperty(prop);
-      return metaProp.getGetterType(prop.getChildren());
+    /**
+     * name=orders[0].item[0].name
+     * 返回orders.item.name
+     * 如果不含有某一个字段，比如order中不含有item字段，则返回orders
+     */
+    public String findProperty(String name) {
+        StringBuilder prop = buildProperty(name, new StringBuilder());
+        return prop.length() > 0 ? prop.toString() : null;
     }
-    // issue #506. Resolve the type inside a Collection Object
-    return getGetterType(prop);
-  }
 
-  private MetaClass metaClassForProperty(PropertyTokenizer prop) {
-    Class<?> propType = getGetterType(prop);
-    return MetaClass.forClass(propType, reflectorFactory);
-  }
-
-  private Class<?> getGetterType(PropertyTokenizer prop) {
-    Class<?> type = reflector.getGetterType(prop.getName());
-    if (prop.getIndex() != null && Collection.class.isAssignableFrom(type)) {
-      Type returnType = getGenericGetterType(prop.getName());
-      if (returnType instanceof ParameterizedType) {
-        Type[] actualTypeArguments = ((ParameterizedType) returnType).getActualTypeArguments();
-        if (actualTypeArguments != null && actualTypeArguments.length == 1) {
-          returnType = actualTypeArguments[0];
-          if (returnType instanceof Class) {
-            type = (Class<?>) returnType;
-          } else if (returnType instanceof ParameterizedType) {
-            type = (Class<?>) ((ParameterizedType) returnType).getRawType();
-          }
+    /**
+     * name=orders[0].item[0].name
+     * 返回orders.item.name
+     * 如果不含有某一个字段，比如order中不含有item字段，则返回orders
+     */
+    public String findProperty(String name, boolean useCamelCaseMapping) {
+        if (useCamelCaseMapping) {
+            name = name.replace("_", "");
         }
-      }
+        return findProperty(name);
     }
-    return type;
-  }
 
-  private Type getGenericGetterType(String propertyName) {
-    try {
-      Invoker invoker = reflector.getGetInvoker(propertyName);
-      if (invoker instanceof MethodInvoker) {
-        Field _method = MethodInvoker.class.getDeclaredField("method");
-        _method.setAccessible(true);
-        Method method = (Method) _method.get(invoker);
-        return TypeParameterResolver.resolveReturnType(method, reflector.getType());
-      } else if (invoker instanceof GetFieldInvoker) {
-        Field _field = GetFieldInvoker.class.getDeclaredField("field");
-        _field.setAccessible(true);
-        Field field = (Field) _field.get(invoker);
-        return TypeParameterResolver.resolveFieldType(field, reflector.getType());
-      }
-    } catch (NoSuchFieldException e) {
-    } catch (IllegalAccessException e) {
+    public String[] getGetterNames() {
+        return reflector.getGetablePropertyNames();
     }
-    return null;
-  }
 
-  public boolean hasSetter(String name) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
-      if (reflector.hasSetter(prop.getName())) {
-        MetaClass metaProp = metaClassForProperty(prop.getName());
-        return metaProp.hasSetter(prop.getChildren());
-      } else {
-        return false;
-      }
-    } else {
-      return reflector.hasSetter(prop.getName());
+    public String[] getSetterNames() {
+        return reflector.getSetablePropertyNames();
     }
-  }
 
-  public boolean hasGetter(String name) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
-      if (reflector.hasGetter(prop.getName())) {
-        MetaClass metaProp = metaClassForProperty(prop);
-        return metaProp.hasGetter(prop.getChildren());
-      } else {
-        return false;
-      }
-    } else {
-      return reflector.hasGetter(prop.getName());
+    /**
+     * 情况1：name=orders[0].item[0].name
+     * 递归找到name的class
+     * 情况2：name=orders[0].item[0]
+     * 递归找到item的class
+     */
+    public Class<?> getSetterType(String name) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            MetaClass metaProp = metaClassForProperty(prop.getName());
+            return metaProp.getSetterType(prop.getChildren());
+        } else {
+            return reflector.getSetterType(prop.getName());
+        }
     }
-  }
 
-  public Invoker getGetInvoker(String name) {
-    return reflector.getGetInvoker(name);
-  }
-
-  public Invoker getSetInvoker(String name) {
-    return reflector.getSetInvoker(name);
-  }
-
-  private StringBuilder buildProperty(String name, StringBuilder builder) {
-    PropertyTokenizer prop = new PropertyTokenizer(name);
-    if (prop.hasNext()) {
-      String propertyName = reflector.findPropertyName(prop.getName());
-      if (propertyName != null) {
-        builder.append(propertyName);
-        builder.append(".");
-        MetaClass metaProp = metaClassForProperty(propertyName);
-        metaProp.buildProperty(prop.getChildren(), builder);
-      }
-    } else {
-      String propertyName = reflector.findPropertyName(name);
-      if (propertyName != null) {
-        builder.append(propertyName);
-      }
+    /**
+     * 情况1：name=orders[0].item[0].name
+     * 递归找到name的class
+     * 情况2：name=orders[0].item[0]
+     * 递归找到item的class
+     */
+    public Class<?> getGetterType(String name) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            MetaClass metaProp = metaClassForProperty(prop);
+            return metaProp.getGetterType(prop.getChildren());
+        }
+        // issue #506. Resolve the type inside a Collection Object
+        return getGetterType(prop);
     }
-    return builder;
-  }
 
-  public boolean hasDefaultConstructor() {
-    return reflector.hasDefaultConstructor();
-  }
+    /**
+     * 本方法获取集合类型的元素类型
+     * 当prop=orders[0].item[0].name
+     * orders属性为List<Order>类型,忽略.item[0].name
+     * 本方法解析Order，用该Order.class创建MetaClass
+     */
+    private MetaClass metaClassForProperty(PropertyTokenizer prop) {
+        Class<?> propType = getGetterType(prop);
+        return MetaClass.forClass(propType, reflectorFactory);
+    }
+
+    /**
+     * 为当前类的name属性的rawType创建MetaClass
+     * 用于递归向下查找MetaClass的属性
+     */
+    public MetaClass metaClassForProperty(String name) {
+        Class<?> propType = reflector.getGetterType(name);
+        return MetaClass.forClass(propType, reflectorFactory);
+    }
+
+    /**
+     * 本方法获取集合类型的元素类型
+     * 情况1：当prop=orders[0].item[0].name
+     * orders属性为List<Order>类型
+     * 本方法解析Order，忽略.item[0].name等信息
+     * 情况2：当prop=order
+     * order为非集合类型：Order时
+     * 此方法直接返回Order
+     * 根据外部调用情况，通常prop没有子属性的时候才会进来，即不会出现情况1
+     */
+    private Class<?> getGetterType(PropertyTokenizer prop) {
+        Class<?> type = reflector.getGetterType(prop.getName());
+        if (prop.getIndex() != null && Collection.class.isAssignableFrom(type)) {
+            Type returnType = getGenericGetterType(prop.getName());
+            if (returnType instanceof ParameterizedType) {
+                Type[] actualTypeArguments = ((ParameterizedType) returnType).getActualTypeArguments();
+                if (actualTypeArguments != null && actualTypeArguments.length == 1) {
+                    returnType = actualTypeArguments[0];
+                    if (returnType instanceof Class) {
+                        type = (Class<?>) returnType;
+                    } else if (returnType instanceof ParameterizedType) {
+                        type = (Class<?>) ((ParameterizedType) returnType).getRawType();
+                    }
+                }
+            }
+        }
+        return type;
+    }
+
+    /**
+     * 获取含有泛型信息的属性Type，比class更丰富的信息
+     * 其实本方法可以下沉到reflector中，并缓存起来该propertyName的type
+     * 调用TypeParameterResolver解析属性类型详情
+     */
+    private Type getGenericGetterType(String propertyName) {
+        try {
+            Invoker invoker = reflector.getGetInvoker(propertyName);
+            if (invoker instanceof MethodInvoker) {
+                Field _method = MethodInvoker.class.getDeclaredField("method");
+                _method.setAccessible(true);
+                Method method = (Method) _method.get(invoker);
+                return TypeParameterResolver.resolveReturnType(method, reflector.getType());
+            } else if (invoker instanceof GetFieldInvoker) {
+                Field _field = GetFieldInvoker.class.getDeclaredField("field");
+                _field.setAccessible(true);
+                Field field = (Field) _field.get(invoker);
+                return TypeParameterResolver.resolveFieldType(field, reflector.getType());
+            }
+        } catch (NoSuchFieldException e) {
+        } catch (IllegalAccessException e) {
+        }
+        return null;
+    }
+
+    /**
+     * name=orders[0].item[0].name
+     * 递归向下遍历每一个属性：orders、item、name是否都存在setter方法
+     */
+    public boolean hasSetter(String name) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            if (reflector.hasSetter(prop.getName())) {
+                MetaClass metaProp = metaClassForProperty(prop.getName());
+                return metaProp.hasSetter(prop.getChildren());
+            } else {
+                return false;
+            }
+        } else {
+            return reflector.hasSetter(prop.getName());
+        }
+    }
+
+    /**
+     * name=orders[0].item[0].name
+     * 递归向下遍历每一个属性：orders、item、name是否都存在setter方法
+     */
+    public boolean hasGetter(String name) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            if (reflector.hasGetter(prop.getName())) {
+                MetaClass metaProp = metaClassForProperty(prop);
+                return metaProp.hasGetter(prop.getChildren());
+            } else {
+                return false;
+            }
+        } else {
+            return reflector.hasGetter(prop.getName());
+        }
+    }
+
+    public Invoker getGetInvoker(String name) {
+        return reflector.getGetInvoker(name);
+    }
+
+    public Invoker getSetInvoker(String name) {
+        return reflector.getSetInvoker(name);
+    }
+
+    /**
+     * name=orders[0].item[0].name
+     * 返回orders.item.name
+     * 如果不含有某一个字段，则停止，比如order中不含有item字段，则返回orders
+     */
+    private StringBuilder buildProperty(String name, StringBuilder builder) {
+        PropertyTokenizer prop = new PropertyTokenizer(name);
+        if (prop.hasNext()) {
+            String propertyName = reflector.findPropertyName(prop.getName());
+            if (propertyName != null) {
+                builder.append(propertyName);
+                builder.append(".");
+                MetaClass metaProp = metaClassForProperty(propertyName);
+                metaProp.buildProperty(prop.getChildren(), builder);
+            }
+        } else {
+            String propertyName = reflector.findPropertyName(name);
+            if (propertyName != null) {
+                builder.append(propertyName);
+            }
+        }
+        return builder;
+    }
+
+    public boolean hasDefaultConstructor() {
+        return reflector.hasDefaultConstructor();
+    }
 
 }
