36df6346cf44620fd03aaf2a07b475f630454636
j782282271 <782282271@qq.com>
2019-06-29 21:16:22 +0800

    注释

--- src/main/java/org/apache/ibatis/myalgorithm/Solution.java
+++ src/main/java/org/apache/ibatis/myalgorithm/Solution.java
@@ -10,129 +10,116 @@ import java.util.List;
  */
 public class Solution {
     public static void main(String[] args) {
-        int res = shoppingOffers(Arrays.asList(2, 5), Arrays.asList(Arrays.asList(3, 0, 5), Arrays.asList(1, 2, 10)), Arrays.asList(3, 2));
+        Solution s = new Solution();
+        List<List<Integer>> sp = new ArrayList<List<Integer>>();
+        sp.add(Arrays.asList(1, 5, 5, 1, 4, 0, 18));
+        sp.add(Arrays.asList(3, 3, 6, 6, 4, 2, 32));
+        int res = s.shoppingOffers(Arrays.asList(4, 3, 2, 9, 8, 8), sp, Arrays.asList(6, 5, 5, 6, 4, 1));
         System.out.println(res);
     }
 
-    public static int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-        if (isAll0(needs)) {
-            //全为0，直接返回0，递归出口
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        int p = allToCode(price);
+        List<GiftPack> giftPacks = toGiftPackList(special);
+        int n = allToCode(needs);
+        if (p == 0 || n == 0) {
+            //全为0，直接返回0
             return 0;
         }
-        int oneNot0Index = oneNot0Index(needs);
-        if (oneNot0Index != -1) {
-            //只有一个元素不为0则直接计算结果，不需要使用大礼包，也是递归出口
-            return needs.get(oneNot0Index) * price.get(oneNot0Index);
-        }
+        giftPacks.addAll(price2GiftPack(price));
+        return shoppingOffers(giftPacks, n, price.size(), 0);
+    }
 
-        special = reform(special);
-        int minRest = Integer.MAX_VALUE;
-        int val = 0;
-        for (int i = 0; i < special.size(); ++i) {
-            List<Integer> needTemp = new ArrayList<Integer>(needs);
-            int valTemp = calPrice(special.get(i), needTemp);
-            if (valTemp == -1) {
+    public int shoppingOffers(List<GiftPack> giftPacks, int needsCode, int productSize, int start) {
+        if (needsCode == 0) {
+            return 0;
+        }
+        int val = initVal(giftPacks, productSize, needsCode);
+        int needsCodeTemp = needsCode;
+        for (int i = start; i < giftPacks.size(); ++i) {
+            GiftPack gp = giftPacks.get(i);
+            if (!isBiggerThanThreadjold(gp.code, needsCodeTemp, productSize)) {
                 continue;
             }
-            int rest = getPrice(price, needTemp);
-            if (minRest > rest) {
-                minRest = rest;
-                val = valTemp;
-            }
-        }
-
-        val = val + minRest;
-        return val;
-    }
-
-    public static List<List<Integer>> deepCopy(List<List<Integer>> special) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>(special.size());
-        for (int i = 0; i < special.size(); ++i) {
-            List<Integer> list = new ArrayList<Integer>();
-            list.addAll(special.get(i));
-            res.add(list);
-        }
-        return res;
-    }
-
-    public static int add(List<List<Integer>> res, int start, List<List<Integer>> special) {
-        int size = res.size();
-        if (start >= size) {
-            return -1;
-        }
-        for (int i = start; i < size; ++i) {
-            label:
-            for (int j = 0; j < special.size(); ++j) {
-                List<Integer> list = new ArrayList<Integer>();
-                for (int k = 0; k < res.get(i).size(); ++k) {
-                    int val = res.get(i).get(k) + special.get(j).get(k);
-                    if (k != res.get(i).size() - 1 && val > 6) {
-                        break label;
-                    }
-                    list.add(val);
-                }
-                res.add(list);
-            }
-        }
-        return res.size();
-    }
-
-    public static List<List<Integer>> reform(List<List<Integer>> special) {
-        List<List<Integer>> res = deepCopy(special);
-        int i = 0;
-        while (i != -1) {
-            i = add(res, i, special);
-        }
-        return res;
-    }
-
-
-    public static int getPrice(List<Integer> price, List<Integer> needs) {
-        int val = 0;
-        for (int i = 0; i < needs.size(); ++i) {
-            val = val + getIndexPrice(price, i, needs.get(i));
+            needsCodeTemp = needsCodeTemp - gp.code;
+            val = Math.min(val, gp.price + shoppingOffers(giftPacks, needsCodeTemp, productSize, i));
+            needsCodeTemp = needsCode;
         }
         return val;
     }
 
-    public static int getIndexPrice(List<Integer> price, int index, int size) {
-        return price.get(index) * size;
-    }
-
-    //只有一个元素不为0则返回该元素的位置
-    private static int oneNot0Index(List<Integer> needs) {
-        int not0Num = 0;
-        int index = -1;
-        for (Integer n : needs) {
-            if (0 != n) {
-                not0Num++;
-                index = n;
-            }
-            if (not0Num > 1) {
-                return -1;
-            }
+    private int initVal(List<GiftPack> giftPacks, int productSize, int needsCode) {
+        int res = 0;
+        int i = productSize;
+        while (i >= 1) {
+            GiftPack gp = giftPacks.get(giftPacks.size() - productSize - 1 + i);
+            int tm = needsCode % 10;
+            res = res + tm * gp.price;
+            needsCode = needsCode / 10;
+            --i;
         }
-        return not0Num == 1 ? index : -1;
+        return res;
     }
 
-    private static boolean isAll0(List<Integer> needs) {
-        for (Integer n : needs) {
-            if (0 != n) {
+    private static class GiftPack {
+        int code;
+        int price;
+    }
+
+    boolean isBiggerThanThreadjold(int test, int threadhold, int productSize) {
+        int i = 1;
+        while (i <= productSize) {
+            int tm = test % 10;
+            int thm = threadhold % 10;
+            if (tm > thm) {
                 return false;
             }
+            test = test / 10;
+            threadhold = threadhold / 10;
+            ++i;
         }
         return true;
     }
 
-    private static int calPrice(List<Integer> giftPack, List<Integer> needs) {
-        for (int i = 0; i < giftPack.size() - 1; ++i) {
-            if (needs.get(i) < giftPack.get(i)) {
-                return -1;
-            }
+    static GiftPack fromList(List<Integer> list) {
+        GiftPack res = new GiftPack();
+        res.code = toCode(list);
+        res.price = list.get(list.size() - 1);
+        return res;
+    }
+
+    static List<GiftPack> toGiftPackList(List<List<Integer>> list) {
+        List<GiftPack> res = new ArrayList<GiftPack>();
+        for (List<Integer> l : list) {
+            res.add(fromList(l));
         }
-        for (int i = 0; i < giftPack.size() - 1; ++i) {
-            needs.set(i, needs.get(i) - giftPack.get(i));
+        return res;
+    }
+
+    static Integer toCode(List<Integer> list) {
+        int res = 0;
+        for (int i = 0; i < list.size() - 1; ++i) {
+            res = (int) (res + list.get(i) * Math.pow(10, list.size() - 2 - i));
         }
-        return giftPack.get(giftPack.size() - 1);
+        return res;
+    }
+
+    static Integer allToCode(List<Integer> list) {
+        int res = 0;
+        for (int i = 0; i < list.size(); ++i) {
+            res = (int) (res + list.get(i) * Math.pow(10, list.size() - 1 - i));
+        }
+        return res;
+    }
+
+    static List<GiftPack> price2GiftPack(List<Integer> price) {
+        List<GiftPack> res = new ArrayList<GiftPack>();
+        for (int i = 0; i < price.size(); ++i) {
+            GiftPack g = new GiftPack();
+            g.code = (int) Math.pow(10, price.size() - 1 - i);
+            g.price = price.get(i);
+            res.add(g);
+        }
+        return res;
     }
 }
